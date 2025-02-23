02b7707e7fa426b9a757b39fab2eb6fe390a99fc
taqquera <taras.tanchynets123@gmail.com>
2025-02-23 16:54:01 +0200

    Added cool-users page

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,16 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+/* GET cool users page. */
+router.get('/cool-users', function(req, res, next) {
+  res.render('cool-users', {
+    userName: 'Taras',
+    users: [
+      { name: 'John Doe', email: 'john.doe@example.com' },
+      { name: 'Jane Smith', email: 'jane.smith@example.com' },
+      { name: 'Peter Jones', email: 'peter.jones@example.com' }
+    ]
+  });
+});
+
 module.exports = router;
