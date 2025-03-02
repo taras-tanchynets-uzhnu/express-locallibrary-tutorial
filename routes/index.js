a7bb5f91853ce120de01b8f7d2f9415458f3a3a6
taqquera <taras.tanchynets123@gmail.com>
2025-03-02 15:19:44 +0200

    lab2

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,15 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+const mongoDB = process.env.MONGODB_URI;
+
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
 var express = require('express');
 var router = express.Router();
 
