a1d1de68a79c3636daac636b01126c917efdf069
taqquera <taras.tanchynets123@gmail.com>
2025-03-02 16:13:29 +0200

    Додано зміни для lab3

---
+++ models/author.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const AuthorSchema = new Schema({
+  first_name: { type: String, required: true, maxlength: 100 },
+  family_name: { type: String, required: true, maxlength: 100 },
+  date_of_birth: { type: Date },
+  date_of_death: { type: Date },
+});
+
+// Virtual for author's URL
+AuthorSchema.virtual('url').get(function () {
+  return '/catalog/author/' + this._id;
+});
+
+// Export model
+module.exports = mongoose.model('Author', AuthorSchema);
--- routes/index.js
+++ routes/index.js
@@ -18,4 +18,5 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
 module.exports = router;
